4d344eaf58a6b6d86b42f203abacd94af33c6f2c
Daniel Van Acker <danielvanacker0@gmail.com>
2020-05-07 13:29:29 -0400

    random month function

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,6 @@
 import random
 from datetime import date, timedelta
+from dateutil.relativedelta import *
 
 def rand(low, high):
     return random.randint(low, high)
@@ -21,9 +22,15 @@ def getRandDate(low, high):
     addDays = rand(0, diff)
     return str(low + timedelta(days=addDays))
 
+# Gets a randome first day of the month between [start, end]
+def getRandFirstDay(start, end):
+    numMonths = (end.year - start.year) * 12 + (end.month - start.month)
+    addMonths = rand(0, numMonths)
+    return start + relativedelta(months=+addMonths)
 
 print(rand(0, 1))
 print(getType())
 print(getRName())
 print(getSegment())
 print(getRandDate(date(1995, 3, 1), date(1995, 3, 31)))
+print(getRandFirstDay(date(1995, 3, 1), date(1996, 3, 31)))
